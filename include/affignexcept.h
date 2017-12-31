2c889df4fc7cb62f14ecd89a14237643306b22d3
John <john.r.kalyan@gmail.com>
2017-12-31 16:55:58 -0700

    moving headers to include

---
+++ include/affignexcept.h
@@ -0,0 +1,19 @@
+/// Name: affignexcept.h
+/// Purpose: Custom exception class
+/// Author: John Kalyan
+/// Copyright: (c) 2017 John Kalyan
+/// License: MIT License
+
+#pragma once
+
+#include <exception>
+
+class AffignException : public std::exception
+{
+public:
+	AffignException(const char* text);
+	const char* what() const;
+private:
+	AffignException();
+	const char* msg;
+};
